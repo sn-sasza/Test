d8299975a61d86c41912851cd8ccce23faca6988
Sasza <sn.sasza@gmail.com>
2019-12-12 14:38:28 +0300

    I've created singleViewApplication with name "HomeWork_1" and added 2 pictures into "Assets.xassets" folder. It's commit #1.

---
+++ HomeWork_1/HomeWork_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork_1
+//
+//  Created by Sasza Niehaj on 12/12/19.
+//  Copyright © 2019 Sasza Niehaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
