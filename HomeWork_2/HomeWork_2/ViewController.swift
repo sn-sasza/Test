e785583901fab2911203b3918c86a0d936611e59
Sasza <sn.sasza@gmail.com>
2019-12-17 18:03:09 +0300

    Commit #1 Update. All before in this project was an error. All are clear now.

---
+++ HomeWork_2/HomeWork_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork_2
+//
+//  Created by Sasza Niehaj on 12/17/19.
+//  Copyright © 2019 Sasza Niehaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
