3d0a615f5e29bd6d587c51713cfc316a37185fb6
Sasza <sn.sasza@gmail.com>
2019-12-17 17:34:33 +0300

    HomeWork_1 has done.

--- HomeWork_1/HomeWork_1/ViewController.swift
+++ HomeWork_1/HomeWork_1/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
+//MARK: - ViewControllerClass
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    //MARK: - LifeCycleMethods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        emailTextField.placeholder = "email"
+        passwordTextField.placeholder = "password"
+        
+        emailTextField.becomeFirstResponder()
     }
-
-
+    
+    //MARK: - Actions
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        print("User did tap login button!")
+    }
+    
+    //MARK: - Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideKeyboard()
+    }
+    
 }
 
+//MARK: - ViewControllerExtensionWithDelegateProtocol
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            hideKeyboard()
+        }
+        return true
+    }
+    
+}
+
+//MARK: - ViewControllerExtension
+extension ViewController {
+    
+    func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
