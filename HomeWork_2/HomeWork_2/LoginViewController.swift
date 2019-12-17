dea007f55fcf1ae58fe3cae2ce7bd68788319c0c
Sasza <sn.sasza@gmail.com>
2019-12-18 01:32:38 +0300

    Commit #4 I've added all actions and buttons and changed types of keyboards.

--- HomeWork_2/HomeWork_2/LoginViewController.swift
+++ HomeWork_2/HomeWork_2/LoginViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
+//MARK: - LoginViewController
 class LoginViewController: UIViewController {
 
+    //MARK: - outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    
+    //MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    //MARK: - actions
+    @IBAction func forgetPasswordButtonPressed(_ sender: Any) {
+        print("forgetPasswordButtonPressed")
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        print("loginButtonPressed")
+    }
+    
+    
+    @IBAction func helpButtonPressed(_ sender: Any) {
+        print("helpButtonPressed")
+    }
 }
--- HomeWork_2/HomeWork_2/SignUpViewController.swift
+++ HomeWork_2/HomeWork_2/SignUpViewController.swift
@@ -8,9 +8,49 @@
 
 import UIKit
 
+//MARK: - SignUpViewController
 class SignUpViewController: UIViewController {
-
+    
+    //MARK: - outlets
+    @IBOutlet weak var buyerCharacteristicSwitch: UISwitch!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    @IBOutlet weak var userAgreementButton: UIButton!
+    
+    
+    //MARK: - instanceTypes
+    enum Buyer: Int {
+        case privatePerson = 0
+        case privateEntrepreneurOrCompany = 1
+    }
+    
+    //MARK: - properies
+    var buyer: Buyer!
+    
+    //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK: - actions
+    @IBAction func buyerCharacteristicSwitchSelected(_ sender: Any) {
+        if buyerCharacteristicSwitch.isOn { buyer = .privateEntrepreneurOrCompany }
+        else { buyer = .privatePerson }
+    }
+    
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        print("signUpButtonPressed")
+    }
+    
+    @IBAction func helpButtonPressed(_ sender: Any) {
+        print("helpButtonPressed")
+    }
+    
+    @IBAction func userAgreementButtonPressed(_ sender: Any) {
+        print("userAgreementButtonPressed")
+    }
+    
 }
